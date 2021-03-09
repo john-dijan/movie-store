5ed04543dd2c328b8eeec162033dda9d72d54331
John <nmamadaliyev@list.ru>
2021-03-09 08:20:28 +0500

    wef

---
+++ js.js
@@ -0,0 +1,112 @@
+function lionking(){
+    document.getElementById('lionking').style.display='flex';
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+}
+
+function mentalist(){
+    document.getElementById('lionking').style.display='none';
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('mentalist').style.display='flex';
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+}
+
+function forsaj(){
+    document.getElementById('watchs').style.display='block'
+    document.getElementById('forsaj7').style.display='flex';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+}
+
+function thor(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('thor').style.display='flex';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+}
+
+function avenger(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('avenger').style.display='flex';
+}
+
+function guardians(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('guardians').style.display='flex';
+}
+
+function charlie(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('charlie').style.display='flex';
+}
+
+function sonic(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('sonic').style.display='flex';
+}
+
+function b13(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('b13').style.display='flex';
+}
+
+function hulk(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('hulk').style.display='flex';
+}
+
+function terabithia(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('terabithia').style.display='flex';
+}
+
+function starwars(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('starwars').style.display='flex';
+}
+
+function avatar(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('avatar').style.display='flex';
+}
+
+function jurassic(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('jurassic').style.display='flex';
+}
+
+function avvenger(){
+    document.getElementById('watchs').style.display='block';
+    document.getElementById('films').style.filter='blur(4px)';
+    document.getElementById('active').style.filter='blur(4px)';
+    document.getElementById('avvenger').style.display='flex';
+}
+
+function yop(){
+    document.getElementById('watchs').style.display='none';
+    document.getElementById('films').style.filter='none';
+    document.getElementById('active').style.filter='none';
+}
