451c5b59e059f6bc2160ca8a76246cbcc4d11218
john-dijan <68785967+john-dijan@users.noreply.github.com>
2021-03-14 21:59:36 +0500

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 function lionking(){
     document.getElementById('lionking').style.display='flex';
     document.getElementById('watchs').style.display='block';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
 }
@@ -10,6 +11,7 @@ function mentalist(){
     document.getElementById('watchs').style.display='block';
     document.getElementById('mentalist').style.display='flex';
     document.getElementById('watchs').style.display='block';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
 }
@@ -18,12 +20,14 @@ function forsaj(){
     document.getElementById('watchs').style.display='block'
     document.getElementById('forsaj7').style.display='flex';
     document.getElementById('films').style.filter='blur(4px)';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
 }
 
 function thor(){
     document.getElementById('watchs').style.display='block';
     document.getElementById('thor').style.display='flex';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
 }
@@ -33,6 +37,7 @@ function avenger(){
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('avenger').style.display='flex';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
 }
 
 function guardians(){
@@ -40,11 +45,13 @@ function guardians(){
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('guardians').style.display='flex';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
 }
 
 function charlie(){
     document.getElementById('watchs').style.display='block';
     document.getElementById('films').style.filter='blur(4px)';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('charlie').style.display='flex';
 }
@@ -54,6 +61,7 @@ function sonic(){
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('sonic').style.display='flex';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
 }
 
 function b13(){
@@ -61,18 +69,21 @@ function b13(){
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('b13').style.display='flex';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
 }
 
 function hulk(){
     document.getElementById('watchs').style.display='block';
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('hulk').style.display='flex';
 }
 
 function terabithia(){
     document.getElementById('watchs').style.display='block';
     document.getElementById('films').style.filter='blur(4px)';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('terabithia').style.display='flex';
 }
@@ -80,6 +91,7 @@ function terabithia(){
 function starwars(){
     document.getElementById('watchs').style.display='block';
     document.getElementById('films').style.filter='blur(4px)';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('starwars').style.display='flex';
 }
@@ -87,12 +99,14 @@ function starwars(){
 function avatar(){
     document.getElementById('watchs').style.display='block';
     document.getElementById('films').style.filter='blur(4px)';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('avatar').style.display='flex';
 }
 
 function jurassic(){
     document.getElementById('watchs').style.display='block';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('films').style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('jurassic').style.display='flex';
@@ -101,6 +115,7 @@ function jurassic(){
 function avvenger(){
     document.getElementById('watchs').style.display='block';
     document.getElementById('films').style.filter='blur(4px)';
+    document.getElementsByClassName('header')[0].style.filter='blur(4px)';
     document.getElementById('active').style.filter='blur(4px)';
     document.getElementById('avvenger').style.display='flex';
 }
@@ -108,5 +123,152 @@ function avvenger(){
 function yop(){
     document.getElementById('watchs').style.display='none';
     document.getElementById('films').style.filter='none';
+    document.getElementsByClassName('header')[0].style.filter='none';
     document.getElementById('active').style.filter='none';
+}
+
+function register(){
+    document.getElementById('active').style.display='none';
+    document.getElementById('films').style.display='none';
+}
+
+function choobas(){
+    document.getElementById('registratsiya').style.display='flex';
+    document.getElementById('basic').style.display='block';
+    document.getElementById('tplan').style.display='none';
+}
+
+function choopre(){
+    document.getElementById('registratsiya').style.display='flex';
+    document.getElementById('premium').style.display='block';
+    document.getElementById('tplan').style.display='none';
+}
+
+function chooprep(){
+    document.getElementById('registratsiya').style.display='flex';
+    document.getElementById('premiumplus').style.display='block';
+    document.getElementById('tplan').style.display='none';
+}
+
+function tariff(){
+    document.getElementById('tplan').style.display='block';
+    document.getElementById('active').style.display='none';
+    document.getElementById('films').style.display='none';
+}
+
+function och(){
+    document.getElementById('active').style.display='block';
+    document.getElementById('films').style.display='block';
+    document.getElementById('not-active').style.display='none';
+    document.getElementById('registratsiya').style.display='none';
+    document.getElementById('aktiv').style.display='flex';
+    document.getElementsByClassName('block')[0].style.display='none';
+    document.getElementsByClassName('block')[1].style.display='none';
+    document.getElementsByClassName('block')[2].style.display='none';
+    document.getElementsByClassName('block')[3].style.display='none';
+    document.getElementsByClassName('block')[4].style.display='none';
+    document.getElementsByClassName('block')[5].style.display='none';
+    document.getElementsByClassName('block')[6].style.display='none';
+    document.getElementsByClassName('block')[7].style.display='none';
+    document.getElementsByClassName('block')[8].style.display='none';
+    document.getElementsByClassName('block')[9].style.display='none';
+    document.getElementsByClassName('block')[10].style.display='none';
+    document.getElementsByClassName('block')[11].style.display='none';
+    document.getElementsByClassName('block')[12].style.display='none';
+    document.getElementsByClassName('block')[13].style.display='none';
+    document.getElementsByClassName('block')[14].style.display='none';
+    document.getElementsByClassName('about')[0].style.display='block';
+    document.getElementsByClassName('about')[1].style.display='block';
+    document.getElementsByClassName('about')[2].style.display='block';
+    document.getElementsByClassName('about')[3].style.display='block';
+    document.getElementsByClassName('about')[4].style.display='block';
+    document.getElementsByClassName('about')[5].style.display='block';
+    document.getElementsByClassName('about')[6].style.display='block';
+    document.getElementsByClassName('about')[7].style.display='block';
+    document.getElementsByClassName('about')[8].style.display='block';
+    document.getElementsByClassName('about')[9].style.display='block';
+    document.getElementsByClassName('about')[10].style.display='block';
+    document.getElementsByClassName('about')[11].style.display='block';
+    document.getElementsByClassName('about')[12].style.display='block';
+    document.getElementsByClassName('about')[13].style.display='block';
+    document.getElementsByClassName('about')[14].style.display='block';
+    document.getElementsByClassName('watch-this')[0].style.display='block';
+    document.getElementsByClassName('premier')[0].style.marginTop='95px';
+}
+
+function otkr(){
+    document.getElementById('not-active').style.display='none';
+    document.getElementById('aktiv').style.display='flex';
+    document.getElementById('active').style.display='block';
+    document.getElementById('films').style.display='block';
+    document.getElementsByClassName('block')[0].style.display='none';
+    document.getElementsByClassName('block')[1].style.display='none';
+    document.getElementsByClassName('block')[2].style.display='none';
+    document.getElementsByClassName('block')[3].style.display='none';
+    document.getElementsByClassName('block')[4].style.display='none';
+    document.getElementsByClassName('block')[5].style.display='none';
+    document.getElementsByClassName('block')[6].style.display='none';
+    document.getElementsByClassName('block')[7].style.display='none';
+    document.getElementsByClassName('block')[8].style.display='none';
+    document.getElementsByClassName('block')[9].style.display='none';
+    document.getElementsByClassName('block')[10].style.display='none';
+    document.getElementsByClassName('block')[11].style.display='none';
+    document.getElementsByClassName('block')[12].style.display='none';
+    document.getElementsByClassName('block')[13].style.display='none';
+    document.getElementsByClassName('block')[14].style.display='none';
+    document.getElementsByClassName('about')[0].style.display='block';
+    document.getElementsByClassName('about')[1].style.display='block';
+    document.getElementsByClassName('about')[2].style.display='block';
+    document.getElementsByClassName('about')[3].style.display='block';
+    document.getElementsByClassName('about')[4].style.display='block';
+    document.getElementsByClassName('about')[5].style.display='block';
+    document.getElementsByClassName('about')[6].style.display='block';
+    document.getElementsByClassName('about')[7].style.display='block';
+    document.getElementsByClassName('about')[8].style.display='block';
+    document.getElementsByClassName('about')[9].style.display='block';
+    document.getElementsByClassName('about')[10].style.display='block';
+    document.getElementsByClassName('about')[11].style.display='block';
+    document.getElementsByClassName('about')[12].style.display='block';
+    document.getElementsByClassName('about')[13].style.display='block';
+    document.getElementsByClassName('about')[14].style.display='block';
+    document.getElementsByClassName('watch-this')[0].style.display='block';
+    document.getElementsByClassName('premier')[0].style.marginTop='95px';
+}
+
+
+function logout(){
+    document.getElementById('aktiv').style.display='none';
+    document.getElementById('not-active').style.display='flex';
+    document.getElementsByClassName('block')[0].style.display='flex';
+    document.getElementsByClassName('block')[1].style.display='flex';
+    document.getElementsByClassName('block')[2].style.display='flex';
+    document.getElementsByClassName('block')[3].style.display='flex';
+    document.getElementsByClassName('block')[4].style.display='flex';
+    document.getElementsByClassName('block')[5].style.display='flex';
+    document.getElementsByClassName('block')[6].style.display='flex';
+    document.getElementsByClassName('block')[7].style.display='flex';
+    document.getElementsByClassName('block')[8].style.display='flex';
+    document.getElementsByClassName('block')[9].style.display='flex';
+    document.getElementsByClassName('block')[10].style.display='flex';
+    document.getElementsByClassName('block')[11].style.display='flex';
+    document.getElementsByClassName('block')[12].style.display='flex';
+    document.getElementsByClassName('block')[13].style.display='flex';
+    document.getElementsByClassName('block')[14].style.display='flex';
+    document.getElementsByClassName('about')[0].style.display='none';
+    document.getElementsByClassName('about')[1].style.display='none';
+    document.getElementsByClassName('about')[2].style.display='none';
+    document.getElementsByClassName('about')[3].style.display='none';
+    document.getElementsByClassName('about')[4].style.display='none';
+    document.getElementsByClassName('about')[5].style.display='none';
+    document.getElementsByClassName('about')[6].style.display='none';
+    document.getElementsByClassName('about')[7].style.display='none';
+    document.getElementsByClassName('about')[8].style.display='none';
+    document.getElementsByClassName('about')[9].style.display='none';
+    document.getElementsByClassName('about')[10].style.display='none';
+    document.getElementsByClassName('about')[11].style.display='none';
+    document.getElementsByClassName('about')[12].style.display='none';
+    document.getElementsByClassName('about')[13].style.display='none';
+    document.getElementsByClassName('about')[14].style.display='none';
+    document.getElementsByClassName('watch-this')[0].style.display='none';
+    document.getElementsByClassName('premier')[0].style.marginTop='135px';
 }
